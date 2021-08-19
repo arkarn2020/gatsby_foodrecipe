dab9336c064d6d3044fb6346f037b69f1801551a
arkarn <arkarn2020@gmail.com>
2021-08-19 20:08:41 +0530

    basic minimal feature

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,44 @@ const Contact = () => {
   return (
     <Layout>
       <title>Contact</title>
-      <h2>this is contact us page</h2>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Love to hear from you!</h3>
+            <h5>Issue with your live order?</h5>
+            <p>
+              Click on the 'Support' or 'Online ordering help' section in your
+              app to connect to our customer support team.
+            </p>
+            <h5>Need support?</h5>
+            <p>
+              Click on the 'Pro help' section in your app to connect to our Pro
+              support team.
+            </p>
+            <h5>Report a Safety Emergency.</h5>
+            <p>We are committed to the safety of everyone.</p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email address</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   );
 };
